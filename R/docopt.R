76a4036e3fb9cbb83779838183c5ebdd2440057e
Edwin de Jonge <edwindjonge@gmail.com>
2013-10-13 13:15:04 +0200

    restructering

--- R/docopt.R
+++ R/docopt.R
@@ -17,3 +17,67 @@ version <- function(version=NULL){
     cat("Version: ", version, "\n")
   }
 }
+
+# parse_doc_options = (doc) ->
+#     (Option.parse('-' + s) for s in doc.split(/^\s*-|\n\s*-/)[1..])
+# 
+# printable_usage = (doc, name) ->
+#     usage_split = doc.split(/(usage:)/i)
+#     if usage_split.length < 3
+#         throw new DocoptLanguageError '"usage:" (case-insensitive) not found.'
+#     else if usage_split.length > 3
+#         throw new DocoptLanguageError 'More than one "usage:" (case-insensitive).'
+#     return usage_split[1..].join('').split(/\n\s*\n/)[0].replace(/^\s+|\s+$/, '')
+# 
+# formal_usage = (printable_usage) ->
+#     pu = printable_usage.split(/\s+/)[1..]  # split and drop "usage:"
+#     ((if s == pu[0] then '|' else s) for s in pu[1..]).join ' '
+# 
+# extras = (help, version, options, doc) ->
+#     opts = {}
+#     opts[opt.name()] = true for opt in options when opt.value
+#     if help and (opts['--help'] or opts['-h'])
+#         print doc.replace /^\s*|\s*$/, ''
+#         process.exit()
+#     if version and opts['--version']
+#         print version
+#         process.exit()
+# 
+# class Dict extends Object
+# 
+#     constructor: (pairs) ->
+#         (@[key] = value for [key, value] in pairs)
+# 
+#     toString: () ->
+#         atts = (k for k of @ when k not in ['constructor', 'toString'])
+#         atts.sort()
+#         '{' + (k + ': ' + @[k] for k in atts).join(',\n ') + '}'
+# 
+# docopt = (doc, kwargs={}) ->
+#     allowedargs = ['argv', 'name', 'help', 'version']
+#     throw new Error "unrecognized argument to docopt: " for arg of kwargs \
+#         when arg not in allowedargs
+# 
+#     argv    = if kwargs.argv is undefined \
+#               then process.argv[2..] else kwargs.argv
+#     name    = if kwargs.name is undefined \
+#               then null else kwargs.name
+#     help    = if kwargs.help is undefined \
+#               then true else kwargs.help
+#     version = if kwargs.version is undefined \
+#               then null else kwargs.version
+# 
+#     usage = printable_usage doc, name
+#     pot_options = parse_doc_options doc
+#     formal_pattern   = parse_pattern formal_usage(usage), pot_options
+# 
+#     argv = parse_args argv, pot_options
+#     extras help, version, argv, doc
+#     [matched, left, argums] = formal_pattern.fix().match argv
+#     if matched and left.length is 0  # better message if left?
+#         options = (opt for opt in argv when opt.constructor is Option)
+#         pot_arguments = (a for a in formal_pattern.flat() \
+#             when a.constructor in [Argument, Command])
+#         parameters = [].concat pot_options, options, pot_arguments, argums
+#         return new Dict([a.name(), a.value] for a in parameters)
+#     throw new DocoptExit usage
---
+++ R/parse.R
@@ -0,0 +1,241 @@
+parse_shorts <- function(tokens, options){
+  raw <- substring(tokens$shift(), 2)
+  parsed <- list()
+  while(nchar(raw) > 0){
+    r <- substr(raw, 1, 1)
+#         opt = (o for o in options when o.short isnt null and o.short[1] == raw[0])
+  opt <- Filter( function(o){
+                 !is.null(o$short) && substring(o$short, 2, 1) == r
+               }
+               , options)
+  if (length(opt) > 1){
+#         if opt.length > 1
+#             tokens.error "-#{raw[0]} is specified ambiguously #{opt.length} times"
+    tokens$error("-", r, " is specified ambiguously ", length(opt), "times")
+  } else if (length(opt) < 1){
+#         if opt.length < 1
+#             if tokens.error is DocoptExit
+#                 throw new tokens.error "-#{raw[0]} is not recognized"
+    tokens$error("-",r," is not recognized")
+  } else {
+#             else
+#                 o = new Option('-' + raw[0], null)
+#                 options.push(o)
+#                 parsed.push(o)
+#                 raw = raw[1..]
+#                 continue
+    o = Option(paste0("-", r), NULL)
+    options <- c(options, o)
+    parsed <- c(parsed, o)
+    raw <- substring(raw, 2)
+    next
+  }
+#         o = opt[0]
+  o = opt[1]
+#         opt = new Option o.short, o.long, o.argcount, o.value
+  opt <- Option(o$short, o$long, o$argcount, o$value)
+#         raw = raw[1..]
+  raw <- substring(raw, 2)
+#         if opt.argcount == 0
+  if (opt$argcount == 0){
+    value <- TRUE
+  } else {
+#             value = true
+#         else
+#             if raw in ['', null]
+#                 if tokens.current() is null
+#                     throw new tokens.error "-#{opt.short[0]} requires argument"
+#                 raw = tokens.shift()
+#             [value, raw] = [raw, '']
+    if (raw == ""){
+      if (is.null(tokens$current())){
+        tokens$error("-", substr(opt$short,1,1)," requires argument")
+      }
+      raw <- tokens$shift()
+      value <- raw
+      raw <- ''
+    }
+  }
+#         opt.value = value
+  opt$value <- value
+#         parsed.push opt
+  parsed <- c(parsed, opt)
+  }
+  parsed
+#     return parsed
+}
+# 
+# 
+# parse_long = (tokens, options) ->
+#     [_, raw, value] = tokens.current().match(/(.*?)=(.*)/) ? [null,
+#                                                 tokens.current(), '']
+#     tokens.shift()
+#     value = if value == '' then null else value
+#     opt = (o for o in options when o.long and o.long[0...raw.length] == raw)
+#     if opt.length > 1
+#         throw new tokens.error "#{raw} is specified ambiguously #{opt.length} times"
+#     if opt.length < 1
+#         if tokens.error is DocoptExit
+#             throw new tokens.error "#{raw} is not recognized"
+#         else
+#             o = new Option(null, raw, +!!value)
+#             options.push(o)
+#             return [o]
+#     o = opt[0]
+#     opt = new Option o.short, o.long, o.argcount, o.value
+#     if opt.argcount == 1
+#         if value is null
+#             if tokens.current() is null
+#                 tokens.error "#{opt.name()} requires argument"
+#             value = tokens.shift()
+#     else if value is not null
+#         tokens.error "#{opt.name()} must not have an argument"
+#     opt.value = value or true
+#     [opt]
+parse_long <- function(tokens, options){
+  stop("Not implemented")
+}
+
+# 
+# 
+# parse_pattern = (source, options) ->
+#     tokens = new TokenStream source.replace(/([\[\]\(\)\|]|\.\.\.)/g, ' $1 '),
+#                          DocoptLanguageError
+#     result = parse_expr tokens, options
+#     if tokens.current() is not null
+#         raise tokens.error 'unexpected ending: ' + tokens.join ' '
+#     new Required result
+parse_pattern <- function(src, options){
+  src <- gsub("([\\(\\)\\|]|\\[|\\]|\\.\\.\\.)", ' \\1 ', src)
+  tokens <- Tokens(src)
+  result <- parse_expr(tokens, options)
+  if (length(tokens$current())){
+    stop("unexpected ending:'", tokens$join(" "),"'")
+  }
+  Required(result)
+  tokens
+}
+
+parse_expr <- function(tokens, options){
+  seq <- parse_seq(tokens, options)
+  if (tokens$current() != "|"){
+    return(seq)
+  }
+  
+  result <- if(length(seq)>1) list(Required(seq)) else seq
+  while(tokens$current() == "|"){
+    tokens$shift()
+    seq <- parse_seq(tokens, options)
+    result <- c(result, if (length(seq)>1)list(Required(seq)) else seq)
+  }
+  
+  if (length(result)>1){
+    list(Either(result))
+  } else result
+}
+
+parse_seq <- function(tokens, options){ 
+# seq ::= ( atom [ '...' ] )* ;
+  result <- list()
+  while(!(tokens$current() %in% c("", "]", ")", "|"))){
+    atom <- parse_atom(tokens, options)
+    if (tokens$current() == '...'){
+      atom <- OneOrMore(atom)
+      tokens$shift()
+    }
+    result <- c(result, atom)
+  }
+  result
+}
+ 
+
+parse_atom <- function(tokens, options){
+  # atom ::= '(' expr ')' | '[' expr ']' | '[' 'options' ']' | '--'
+  #        | long | shorts | argument | command ;
+  token <- tokens$current()
+  result <- list()
+  if (token == '('){
+    tokens$shift()
+    result <- list(Required(parse_expr(tokens, options)))
+    if (tokens$shift() != ")"){
+      tokens$error("Unmatched '('")
+    }
+    result
+  } else if (token == '['){
+    tokens$shift()
+    if (tokens$current() == 'options'){
+      result = list(Optional(list(AnyOptions())))
+      tokens$shift()
+    } else {
+      result <- list(Optional(parse_expr(tokens, options)))
+    }
+    if (tokens$shift() != "]"){
+      tokens$error("Unmatched '['")
+    }
+    result
+  } else if (substr(token, 1, 2) == '--' ){
+    #     else if token[0..1] is '--'
+    #         if token is '--'
+    #             [new Command tokens.shift()]
+    #         else
+    #             parse_long tokens, options
+    if (token == '--'){
+      list(Command(tokens$shift()))
+    } else {
+      parse_long(tokens, options)
+    }
+  } else if (substr(token,1,1) == '-' && token != '-'){
+  #     else if token[0] is '-' and token isnt '-'
+  #         parse_shorts tokens, options
+    parse_shorts(tokens, options)
+  } else if (grepl("^<.+>$", token) || grepl("^[^a-z]*[A-Z]+[^a-z]*$", token)){
+    #     else if (token[0] is '<' and
+    #           token[token.length-1] is '>') or /^[^a-z]*[A-Z]+[^a-z]*$/.test(token)
+    #         [new Argument tokens.shift()]
+    list(Argument(tokens$shift()))
+  } else{
+    #         [new Command tokens.shift()]
+    list(Command(tokens$shift()))
+  }
+}
+
+# 
+# parse_args = (source, options) ->
+parse_args <- function(src, options){
+#     tokens = new TokenStream source, DocoptExit
+  tokens <- Tokens(src, error=stop)
+#     #options = options.slice(0) # shallow copy, not sure if necessary
+#     opts = []
+  opts = list()
+  #     while (token = tokens.current()) isnt null
+  while (!is.null(token <- tokens$current()))
+    #         if token is '--'
+    #             #tokens.shift()
+    #             return opts.concat(new Argument null, tokens.shift() while tokens.length)
+    if (token == '--'){
+      # ????
+      return(c(opts, Argument(NULL, tokens$shift())))
+    } else if (grepl("^--", token)){
+      #         else if token[0...2] is '--'
+      #             long = parse_long tokens, options
+      #             opts = opts.concat long
+      long <- parse_long(tokens, options)
+      opts <- c(opts, long)
+    } else if (grepl("^-.+", token)){
+      #         else if token[0] is '-' and token isnt '-'
+      #             shorts = parse_shorts tokens, options
+      #             opts = opts.concat shorts
+      shorts = parse_shorts(tokens, options)
+      opts <- c(opts, shorts)
+    } else {
+      opts <- c(opts, Argument(null, tokens$shift()))
+    }
+  return(opts)
+}
+
+# parse_doc_options = (doc) ->
+parse_doc_options <- function(doc){
+#     (Option.parse('-' + s) for s in doc.split(/^\s*-|\n\s*-/)[1..])
+#
+  stop("not implemented")
+}
