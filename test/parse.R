bc734797213097fe24a9ddc3eb27db4607cc88b7
Edwin de Jonge <edwindjonge@gmail.com>
2013-11-05 15:54:15 +0100

    removed test file

--- test/parse.R
+++
@@ -1,9 +0,0 @@
-options <- list(a=Option("-a", "-all"), b=Option("-b", "-best"))
-parse_shorts(Tokens("-ab"),options)
-
-parse_atom(Tokens("-c help", cat), options)
-parse_atom(Tokens("[ -a | -b ]"), options)
-parse_atom(Tokens("( -a | -b )"), options)
-parse_atom(Tokens("( -a | -b )"), options)
-
-parse_pattern("test", options)
